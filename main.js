b8c3c2467b77f8f34886e947189d5eba71dce22d
neuralsubnet <neuralsubnet@gmail.com>
2016-07-19 19:11:13 +0100

    Added jQuery and simple Paper.js usage

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('main.js loaded');
+$(document).ready(function () {
+    'use strict';
+    console.log('main.js loaded');
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+
+    paper.view.draw();
+});
