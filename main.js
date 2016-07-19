a4dd2fe8e4109ef24c3028d4c69532fc6ddd74d7
neuralsubnet <neuralsubnet@gmail.com>
2016-07-19 23:43:14 +0100

    Added user input

--- main.js
+++ main.js
@@ -8,15 +8,21 @@ $(document).ready(function () {
     // c.fillColor = 'green';
 
 
-    var c;
+    // var c;
+    // for(var x=25; x<400; x+=50) {
+    //     for(var y=25; y<400; y+=50) {
+    //         c  = Shape.Circle(x, y, 20);
+    //         c.fillColor = 'green';
+    //     }
+    // }
 
-    for(var x=25; x<400; x+=50) {
-        for(var y=25; y<400; y+=50) {
-            c  = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
-    }
 
+    var tool = new Tool();
+
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
 
 
     paper.view.draw();
