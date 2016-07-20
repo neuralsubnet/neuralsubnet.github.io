4910af4f9d052dc0cfd083f969be2df0882c6b6a
neuralsubnet <neuralsubnet@gmail.com>
2016-07-20 13:09:31 +0100

    Added Gulp and Babel transcompile from ES6 to ES5

---
+++ public/es6/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sentences = [
+    {subject: 'Books', verb: 'are', object: 'tough'},
+    {subject: 'Code', verb: 'writes', object: 'itself'}
+];
+
+function say({subject, verb, object}) {
+    console.log(`${subject} ${verb} ${object}`);
+}
+
+for (let s of sentences) {
+    say(s)
+}
